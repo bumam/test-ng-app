1c01dbb8677794c9ed932f7de24c4f457e3cc0f6
Mariia Popleteeva <m.popleteeva@gmail.com>
2020-08-26 15:53:40 +0300

    add menu

---
+++ test-ng-app/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TableComponent } from './table/table.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'pets', component: TableComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ test-ng-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { TableComponent } from './table/table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    TableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
