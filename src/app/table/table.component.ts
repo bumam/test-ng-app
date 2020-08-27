aa0291dbba16da09165d91df0664d4ef7051e3f2
Mariia Popleteeva <m.popleteeva@gmail.com>
2020-08-27 13:19:37 +0300

    small logic

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -16,23 +16,85 @@ export class TableComponent implements OnInit {
   pet: any = {};
   pets = mockData;
   profileForm: FormGroup;
-  btn = 'Создать';
-  head = 'Новый питомец';
   btnClick = false;
+  genders = ['М', 'Ж'];
+  types = ['котяу', 'собакау', 'попугау'];
+
+  MaxValue = (arr) => {
+    let newArr = [];
+    for (var key of arr) {
+      newArr.push(key.id);
+    }
+    return Math.max(...newArr);
+  };
+
+  counter = this.MaxValue(mockData);
 
   constructor(private fb: FormBuilder) {}
 
   initForm() {
     this.profileForm = this.fb.group({
-      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      gender: ['М', [Validators.required]],
+      type: ['котяу', [Validators.required]],
+      color: ['', [Validators.required]],
+      vaccination: ['', [Validators.required]],
     });
   }
 
-  get elFirstName() {
-    return this.profileForm.get('firstName');
+  get name() {
+    return this.profileForm.get('name');
+  }
+
+  get color() {
+    return this.profileForm.get('color');
+  }
+
+  get vaccination() {
+    return this.profileForm.get('vaccination');
+  }
+
+  get type() {
+    return this.profileForm.get('type');
+  }
+
+  get gender() {
+    return this.profileForm.get('gender');
   }
 
   ngOnInit(): void {
     this.initForm();
+  }
+
+  onSubmit() {
+    console.warn(this.profileForm.value);
+    this.initForm();
+  }
+
+  addPet() {
+    this.pet = this.profileForm.value;
+    this.pet.id = ++this.counter;
+    this.pets.push(this.pet);
+  }
+
+  add2Pet(id) {
+    console.log(111);
+    let start = this.pets.findIndex((pet) => pet.id === id);
+    let toRemove = 1;
+    let newArr = this.pets.splice(start, toRemove, ...this.pets);
+    this.pets = newArr;
+    console.log(this.pets);
+  }
+
+  deletePet(id) {
+    this.pets.splice(
+      this.pets.findIndex((pet) => pet.id === id),
+      1
+    );
+  }
+
+  editPet(id) {
+    let obj = this.pets.find((pet) => pet.id === id);
+    this.profileForm.patchValue(obj);
   }
 }
